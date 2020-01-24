cd6e25e04e4066ea0da86973fcde9ac0987383b4
chan270 <nijjarc@sheridancollege.ca>
2020-01-24 15:17:35 -0500

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -9,24 +9,42 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author 
+ * @author Chanvir Nijjar
  */
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        Scanner reader = new Scanner(System.in);
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            magicHand[i]=new Card();
+            magicHand[i].setValue((int)(Math.random()*13)+1);
+            magicHand[i].setSuit(Card.SUITS[((int)(Math.random()*3))]);
+                   
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        System.out.print("Enter a card value (1-13): ");
+        int cardValue = Integer.parseInt(reader.nextLine());
+        System.out.print("Enter a card suit: ");
+        String suit = reader.nextLine();
+        Card card = new Card();
+        card.setSuit(suit);
+        card.setValue(cardValue);
+        String userSuit=card.getSuit();
+        int userValue=card.getValue();
+        
+        for (int i=0;i<magicHand.length;i++){
+            if (magicHand[i].getSuit()==userSuit && magicHand[i].getValue()==userValue){
+                System.out.println("You have matched with the magic hand!");
+            }
+            }
+        }
     }
     
-}

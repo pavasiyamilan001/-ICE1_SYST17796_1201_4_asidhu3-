27cf5dc807da6067511625fd7a267feca11048fd
kaur7782 <kaur7782@sheridancollege.ca>
2020-01-24 15:35:53 -0500

    Finished adding code for 3 and 4 to CardTrick.java”.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,21 +11,60 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author 
  */
+
+/**
+ * 
+ * @author Jasleen Kaur (991565618)
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 public class CardTrick {
+    
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Random _random=new Random();
+        int[] Comp=new int[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            //Comp[i]=_random.nextInt((13)+1);
+            //System.out.println(Comp[i]);
+
+            c.setValue(_random.nextInt((13)+1));
+            c.setSuit(Card.SUITS[_random.nextInt(3)]);
+            
+          
         }
         
         //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
+        Scanner input1=new Scanner(System.in);
+        System.out.print("Enter the suite of your choice.: ");
+        String card_suite;
+        card_suite = input1.nextLine();
+        
+        Scanner input2=new Scanner(System.in);
+        System.out.println("\nEnter the number of your choice.: ");
+        int card_value=input2.nextInt();
+        
+        String[] your_hand=new String[2];
+        your_hand[0]=card_suite;
+        your_hand[1]=String.valueOf(card_value);
+     
+        
+        // Comparing the arrays
+        for(int i=0;i<magicHand.length;i++)
+        {
+            if (your_hand[i]==magicHand[i]){
+            System.out.println("Found!");}
+            else
+             System.out.println("Not Found!");
+        }
+        
         //Then report the result here
     }
     

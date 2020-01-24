088678bffe1b2b0479190cf369562a74edb44680
AlphaKy <60014097+AlphaKy@users.noreply.github.com>
2020-01-24 15:44:58 -0500

    Update CardTrick.java
    Hard-coded luckyCard

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -39,7 +39,7 @@ public class CardTrick {
         
         Card LuckyCard = new Card();
         int random = (int)(Math.random() * 13 + 1);
-        System.out.println(LucyCard);
+        System.out.println("LucyCard");
         
         
     }

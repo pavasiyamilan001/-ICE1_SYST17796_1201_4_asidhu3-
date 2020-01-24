cfe387c6714ad022fdf0e07f7a7bf91854578976
Pratimkataria520 <katariap@sheridancollege.ca>
2020-01-24 15:29:36 -0500

    "Finished adding code for 3 and 4 to CardTrick.java"

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -2,8 +2,10 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *Student number- 991557189
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -20,8 +22,26 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            
+           c.setValue((int) Math.round(Math.random()*13));
+           
+        c.setSuit(Card.SUITS[(int) Math.round(Math.random()*3)]);
+            magicHand[i]= c;
+              
+             }
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number from 1 to 13 ");
+        
+        int number1 = input.nextInt();
+        for(int i=0; i<magicHand.length;i++){
+            
+        if(number1=magicHand[i]){
+            System.out.println("you find");
+        
+        } else {
+            System.out.println("you do not find");
+            }
         }
         
         //insert code to ask the user for Card value and suit, create their card
